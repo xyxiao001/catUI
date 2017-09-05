1f464f03cfb4e6c512f0bd5336d91a5ed3ab3077
xyxiao001 <463618622@qq.com>
2017-09-05 18:50:11 +0800

    修复input 的阴影

--- example/src/components/cat/Input/Input.vue
+++ example/src/components/cat/Input/Input.vue
@@ -131,6 +131,7 @@ export default {
        border-radius: 4px;
        transition: all .3s;
        box-sizing: border-box;
+       -webkit-appearance: none;
 
        &::-moz-placeholder {
          color: $G70;
